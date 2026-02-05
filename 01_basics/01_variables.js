40e3a6458b19dbc209328f1fd7f50b6c879e329e
ETHREALised <surya9ium@gmail.com>
2026-02-05 15:06:02 +0000

    commit confirmed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+let word="Satish"
+const words=["Suryansh","Praveen", "Ankur", "Sahil", "Shrishti"]
+//let word
+word=words[2]
+
+console.log(word)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// number : -2^53 to 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => let age // we did not assign value
+// symbol => unique
+
+
+//objects
+
+console.log(typeof "suryansh")
