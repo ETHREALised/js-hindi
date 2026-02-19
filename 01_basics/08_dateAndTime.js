7a386926bcb832131423cce4e5291d5cdf3947e7
ETHREALised <surya9ium@gmail.com>
2026-02-19 13:54:17 +0000

    commit

---
+++ 01_basics/08_dateAndTime.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+
+console.log(myDate);
+
+console.log(myDate.toString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleString())
+
+console.log(myDate.toLocaleDateString());
+
+
+
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+const myArray = new Array(1,2,3,4,5)
+// myArray.push(6)
+// console.log(myArray);
+// myArray.pop()
+// console.log(myArray)
+// myArray.unshift(11)
+// console.log(myArray);
+// myArray.shift()
+// console.log(myArray);
+console.log(myArray.includes(9));
+
+console.log(myArray.indexOf(19));
+
+
+
+
