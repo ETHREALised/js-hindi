814cafd4bdb1eca8c02efaf724c51680eb9f3992
ETHREALised <surya9ium@gmail.com>
2026-02-19 14:06:20 +0000

    commit

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -11,6 +11,18 @@ console.log(myArray.includes(9));
 
 console.log(myArray.indexOf(19));
 
+// ---Slice and Splicing the array ---
+
+const array1= new Array(1,2,12,23,45)
+console.log(array1);
+
+const array2 = array1.slice(1,3)
+console.log(array2);
+
+const array3 = array1.splice(1,3)
+console.log(array3);
+console.log(array1);
+
 
 
 
