9da74c806ac0933a606fc22385cd7ec69a6373fd
ETHREALised <surya9ium@gmail.com>
2026-02-27 10:54:16 +0000

    commit

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,11 @@
+const user= {
+    name:"Suryansh",
+    age: 20,
+    gender: "male",
+    mobNo: 7979971318
+}
+
+// const user2 = Object.create()
+
+console.log(user.name)
+console.log(user["name"]);
